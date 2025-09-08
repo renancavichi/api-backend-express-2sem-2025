3ad43a81b3b897caf5af31c049d87abc6f76133d
Renan Cavichi <renancavichi@gmail.com>
2025-09-08 17:54:51 -0300

    add cors e avatar

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import profileRouter from './routers/profileRouter.js'
 import productRouter from './routers/productRouter.js'
 import supplierRouter from './routers/supplierRouter.js'
 import customerRouter from './routers/customerRouter.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3333
 
+app.use(cors())
 app.use(express.json()) // Converter o JSON que chegou na requisição em um objeto js e vai salvar em req.body
 
 app.use('/profile', profileRouter)
