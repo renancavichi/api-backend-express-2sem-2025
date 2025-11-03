087e2ca68eb6c5a968da484191d57a0f43ab5b44
Renan Cavichi <renancavichi@gmail.com>
2025-11-03 15:52:44 -0300

    refatoração para salvar hash seguro da senha do user no banco de dados

--- src/controllers/profile/createProfileController.js
+++ src/controllers/profile/createProfileController.js
@@ -1,7 +1,11 @@
 import { create } from '../../models/profileModel.js'
+import bcrypt from 'bcrypt'
 
 export const createProfileController = async (req, res) => {
   const profile = req.body
+
+  profile.pass = await bcrypt.hash(profile.pass, 10)
+
   const result = await create(profile)
   res.json({
     message: 'Usuário criado com sucesso!',
--- src/models/profileModel.js
+++ src/models/profileModel.js
@@ -4,29 +4,60 @@ const prisma = new PrismaClient()
 
 export const create = async (profile) => { 
     return await prisma.user.create({
-        data: profile
+        data: profile,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
     })
 }
 
 export const update = async (id, profile) => {
     return await prisma.user.update({
         data: profile,
-        where: { id }  
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }  
     })
 }
 
 export const list = async () => {
-    return await prisma.user.findMany()
+    return await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
+    })
 }
 
 export const getById = async (id) => {
     return await prisma.user.findUnique({
-        where: { id }
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
     })
 }
 
 export const remove = async (id) => {
     return await prisma.user.delete({
-        where: { id }
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true
+        }
     })
 }
