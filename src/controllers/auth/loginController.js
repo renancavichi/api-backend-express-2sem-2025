dad6f44338355c2b1264cd8fbe6714b799ac0318
Renan Cavichi <renancavichi@gmail.com>
2025-11-10 16:01:01 -0300

    cria o midleware authenticator

--- src/controllers/auth/loginController.js
+++ src/controllers/auth/loginController.js
@@ -15,7 +15,6 @@ export const loginController = async (req, res) => {
 
     const passOk = await bcrypt.compare(pass, user.pass)
     if (!passOk) {
-        console.error('Senha inválida')
         return res.status(401).json({ message: 'Email ou Senha Inválida' })
     }
 
---
+++ src/middlewares/authenticator.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+export const authenticator = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Não autorizado!' })
+    }
+    const token = authHeader.split(' ')[1] // Bearer <token>
+    if(!token){
+        return res.status(401).json({ message: 'Não autorizado!' })
+    }
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.userLogged = payload
+    } catch (err) {
+        console.error('Erro ao verificar o token:', err)
+        return res.status(401).json({ message: 'Token inválido!' })
+    }
+    next()
+}
